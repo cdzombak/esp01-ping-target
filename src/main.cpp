2aebaa74baa6e336d91cefda386788d93450ca5a
Chris Dzombak <chris@dzombak.com>
2024-11-15 13:26:14 -0500

    minor improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h> // https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
-#include "config.h"
 
-#define SERIAL_BAUD 115200
+#include "config.h"
 
 void setup() {
     WiFi.mode(WIFI_STA);
     WiFi.hostname(CFG_HOSTNAME);
     WiFi.begin(CFG_WIFI_ESSID, CFG_WIFI_PASSWORD);
-    yield();  // esp8266 yield allows WiFi stack to run
+    while (WiFi.status() != WL_CONNECTED) {
+        yield();
+        delay(500);
+    }
 }
 
-void loop() {
-    delay(500);
-    yield();
-}
+void loop() {}
